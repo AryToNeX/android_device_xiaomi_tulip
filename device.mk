e9a8a0e076cc0e61643c481bbb1722da4ff60ea5
Naomi Calabretta <me@arytonex.pw>
2019-04-14 19:16:22 +0200

    tulip: Build NoCutoutOverlay
    Change-Id: I7a5a832590959861768672bc2c90389d660590ee

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+# NoCutout
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
